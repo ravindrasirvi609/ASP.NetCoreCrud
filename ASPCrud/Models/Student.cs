ab55de6c91f3e6bfbb25c8c9b667d74a52fdd8b7
rc <sirviravindra609@gmail.com>
2023-04-08 02:13:29 +0530

    Student Class is Created

---
+++ ASPCrud/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPCrud.Models
+{
+	public class Student
+	{
+        [Key]
+		public int id { get; set; }
+
+        public string? name { get; set; }
+
+        public string? course { get; set; }
+
+    }
+}
+
---
+++ ASPCrud/Models/StudentDbContex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCrud.Models
+{
+	public class StudentDbContex : DbContext
+	{
+		public StudentDbContex(DbContextOptions options ):base(options)
+		{
+		}
+
+		public DbSet<Student> students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+			optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
+
